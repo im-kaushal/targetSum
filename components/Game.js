3beff9c754a53dd28cafdfe9651d988d185a866a
im-kaushal <mail4kaushal.kr@gmail.com>
2023-01-31 13:47:53 +0530

    Fun Game App

---
+++ components/Game.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import RandomNumber from './RandomNumber';
+// target = 20 + Math.floor(30 * Math.random());
+
+const Game = props => {
+  const randomNumbers = Array.from({length: props.randomNumberCount}).map(
+    () => 1 + Math.floor(10 * Math.random()),
+  );
+
+  console.log(`Hey ${randomNumbers}`);
+
+  target = randomNumbers
+    .slice(0, props.randomNumberCount - 2)
+    .reduce((acc, curr) => acc + curr, 0);
+
+  // TODO: Suffle the numbers
+  console.log(`Hi ${target}`);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.target}> {target} </Text>
+      <View style={styles.randomContainer}>
+        {randomNumbers.map(
+          (randomNumber, index) => (
+            <RandomNumber key={index} number={randomNumber} />
+          ),
+          /* <Text style={styles.random} key={index}>
+            {randomNumber}
+          </Text> */
+        )}
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ddd',
+    flex: 1,
+    paddingTop: 30,
+  },
+  target: {
+    fontSize: 50,
+    backgroundColor: '#bbb',
+    textAlign: 'center',
+    margin: 50,
+  },
+  randomContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+});
+
+export default Game;
---
+++ components/RandomNumber.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+const RandomNumber = props => {
+  handlePress = () => {
+    console.log(props.number);
+  };
+  console.log(`Fuck,${props.number}`);
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <Text style={styles.random}>{props.number}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  random: {
+    backgroundColor: '#999',
+    width: 100,
+    marginHorizontal: 15,
+    marginVertical: 25,
+    fontSize: 35,
+    textAlign: 'center',
+  },
+});
+
+export default RandomNumber;
